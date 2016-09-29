4ec19452625bd1caf4c6d3f7d2d2fc807d575b89
Kushagra1995 <pkush2310@gmail.com>
2016-09-29 14:05:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,56 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Hi this is artciicle 1',
+    head: 'article 1',
+    date: 'spt 5',
+    content: `  
+                <p>
+                    This is content fo my fistrt artcle.
+                </p>
+                <p>
+                    This is content fo my fistrt artcle.
+                </p>
+                <p>
+                    This is content fo my fistrt artcle.
+                </p>`
+};
+function createTemp(data){
+var title= data.title;
+var head=data.head;
+var content= data.content;
+var date=data.date;
+var htmltemp =`
+  <!doctype html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+      <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <hr/>
+            <h3>${head}</h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+            ${content}
+            </div>
+        </div>
+    </body>
+</html>
+  
+`;
+return htmlTemp;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -14,7 +64,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+  res.send(createTemp(articleOne));
 });
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
